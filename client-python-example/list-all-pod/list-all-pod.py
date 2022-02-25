f3a0288c6590419ae7e8f6d0170ea19c629a47d5
Your Name <you@example.com>
2022-02-25 17:05:47 +0800

    add table dev python-client

--- client-python-example/list-all-pod/list-all-pod.py
+++ client-python-example/list-all-pod/list-all-pod.py
@@ -1,6 +1,8 @@
 #!/usr/local/bin/python3.8
 from kubernetes import client, config
 
+#https://github.com/kubernetes-client/python/blob/master/kubernetes/docs/V1PodStatus.md
+#具体可以安装对应的表结构获取自己想要的功能
 
 def main():
     config.load_kube_config()
@@ -9,8 +11,8 @@ def main():
     print("Listing pods with their IPs:")
     ret = v1.list_pod_for_all_namespaces(watch=False)
     for i in ret.items:
-        print("%s\t%s\t%s" %
-              (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+        print("%s\t%s\t%s\t%s\t%s" %
+              (i.status.host_ip, i.status.pod_ip, i.status.phase, i.metadata.namespace, i.metadata.name))
 
 
 if __name__ == '__main__':
